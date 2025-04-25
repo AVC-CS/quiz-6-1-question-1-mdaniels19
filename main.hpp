5fae1eb91e5250c382edfd4d94db08332ddd5de3
Moth <catlovermittens344@gmail.com>
2025-04-25 15:50:12 -0700

    main done

--- main.hpp
+++ main.hpp
@@ -1,3 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void getTwoValues(int &, int &);
+void getNextPrime();
+void getPrevPrime();
+
+void getTwoValues(int &begin, int &end){
+
+    cout << "insert 2 numbers" << endl;
+    
+    cin >> begin >> end;
+    
+    for (int i=0; i <= 3; i++) //counter: should allow only for the function to be played twice if input is not desirable.
+    
+    if (begin < end){ ///setting desired condition
+    
+    }
+    else{
+        cout << "try again"<< endl;
+   }
+           
+}
+
+void getNextPrime(int begin){
+    
+
+}
+    
+
+
 
 //********************
 // Declare Function Prototypes
